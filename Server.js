32106fb277b35fe64e446632f69adc6237926980
[Carlos Loaiza] <[cloaiza928@gmail.com]>
2024-06-06 21:10:50 -0500

    Express Running

---
+++ Server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port, function () {
+    console.log("El server corre en el puerto: " + port);
+});
+
+app.get('/', function (req, res) {
+    res.send("Hola Mundo!");
+});
